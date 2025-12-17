25b4e91040659cb73e58c0a5bc8e6c8460f3712b
guiassuncao2007 <gui.assuncao2007@gmail.com>
2025-12-17 16:10:32 +0000

    Commit

---
+++ pWeber.m
@@ -0,0 +1,24 @@
+clear
+
+resposta=menu('Opções','Carregar dados','Mostrar dados',...
+    'Efetuar cálculos','Mostrar a localização','Sair do programa');
+if resposta==0
+    while resposta==0
+        fprintf(2,'Erro. Por favor introduza uma opção.\n')
+        resposta=menu('Opções','Carregar dados','Mostrar dados',...
+            'Efetuar cálculos','Mostrar a localização','Sair do programa');
+    end
+else
+    switch resposta
+        case 1
+            % Carregar dados
+        case 2
+            % Mostrar dados
+        case 3
+            % Fazer o calculos
+        case 4
+            % Mostrar localização
+        case 5
+            return;
+    end
+end
