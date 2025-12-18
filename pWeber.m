e8a45cbf06735520876976d26ac525507a8a00c3
guiassuncao2007 <gui.assuncao2007@gmail.com>
2025-12-18 14:02:35 +0000

    pWeber

--- pWeber.m
+++ pWeber.m
@@ -1,28 +1,19 @@
 clear
-
-resposta=menu('Opções','Carregar dados','Mostrar dados',...
-    'Efetuar cálculos','Mostrar a localização','Sair do programa');
-if resposta==0
-    while resposta==0
-        fprintf(2,'Erro. Por favor introduza uma opção.\n')
-        resposta=menu('Opções','Carregar dados','Mostrar dados',...
-            'Efetuar cálculos','Mostrar a localização','Sair do programa');
-    end
-else
-    while ~(resposta==0)
-        resposta=menu('Opções','Carregar dados','Mostrar dados',...
-            'Efetuar cálculos','Mostrar a localização','Sair do programa');
-        switch resposta
-            case 1
-                % Carregar dados
-            case 2
-                % Mostrar dados
-            case 3
-                % Fazer o calculos
-            case 4
-                % Mostrar localização
-            case 5
-                return;
-        end
+resposta=1;
+while ~(resposta==0)
+    resposta=menu('Opções','Carregar dados','Mostrar dados',...
+        'Efetuar cálculos','Mostrar a localização','Sair do programa');
+    switch resposta
+        case 1
+            PedirFicheiro()
+            %Carregar dados
+        case 2
+            % Mostrar dados
+        case 3
+            % Fazer o calculos
+        case 4
+            % Mostrar localização
+        case 5
+            return;
     end
 end
