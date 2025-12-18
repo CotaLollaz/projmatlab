05b5491679e4df7b192f256954be0bdd0e11f74f
joaodomingos29 <joaodomingos640@gmail.com>
2025-12-18 15:17:25 +0000

    mostrar localizacao

--- mostrar_dados.m
+++ mostrar_dados.m
@@ -1,10 +1,8 @@
-function [] = mostrar_dados(Xi,Yi,Qi,Ci)
+function [] = mostrar_dados(xi,yi,Qi,Ci,di)
 %mostrar_dados apresenta os dados carregados
 % Esta função recebe os dados de coordenadas dos pontos de interesse, as
 % quantidades transportadas em cada ligação e o custo unitário para cada
 % ligação
 
-fprintf(['As coordenadas do ponto de interesse são: (%d,%d)\n' ...
-    'As quantidades a transportar em cada ligação são: %d\n' ...
-    'O custo unitário é: %d\n'],Xi,Yi,Qi,Ci)
+fprintf('(%.1f, %.1f)\t|%.1f\t| %.1f\t| %.3f\n',xi,yi,Qi,Ci,di)
 end
--- mostrar_loc.m
+++ mostrar_loc.m
@@ -1,8 +1,8 @@
-function [] = mostrar_loc(X,Y)
+function [] = mostrar_loc(coord)
 %mostrar_loc apresenta a localização calculada
 % Esta função recebe as coordenadas calculadas como resolução do problema
 % de Weber
 
 fprintf(['Resolução do Problema de Weber\n Coordenadas: X = %d, ' ...
-    'Y = %d\n'],X,Y)
+    'Y = %d\n'],coord(1),coord(2))
 end
