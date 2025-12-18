95b6e992772ba7eb9819c7d7eedf36156b7ac554
guiassuncao2007 <gui.assuncao2007@gmail.com>
2025-12-18 14:50:53 +0000

    Pedir Ficheiro v3

--- PedirFicheiro.m
+++ PedirFicheiro.m
@@ -6,7 +6,7 @@ while confirmacao<=0
     ficheiro=input('Introduza o nome comleto do seu ficheiro: ', 's');
     confirmacao=fopen(ficheiro);
     if confirmacao==-1
-        fprintf('Ficheiro não encontrado. Tente novamente.\n');
+        fprintf(2,'Ficheiro não encontrado. Tente novamente.\n');
     else
         fprintf('Ficheiro aberto com sucesso.')
     end
