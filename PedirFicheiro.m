bada9de9d398c2dd748963a91d32b2f45732c6bb
guiassuncao2007 <gui.assuncao2007@gmail.com>
2025-12-18 14:43:50 +0000

    Pedir Ficheiro v2

--- PedirFicheiro.m
+++ PedirFicheiro.m
@@ -1,3 +1,13 @@
-function ficheiro=PedirFicheiro
+function ficheiro=PedirFicheiro()
+
+confirmacao=0;
+
+while confirmacao<=0
     ficheiro=input('Introduza o nome comleto do seu ficheiro: ', 's');
+    confirmacao=fopen(ficheiro);
+    if confirmacao==-1
+        fprintf('Ficheiro não encontrado. Tente novamente.\n');
+    else
+        fprintf('Ficheiro aberto com sucesso.')
+    end
 end
