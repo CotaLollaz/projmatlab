1045f200534f5e52e6af9742464787a49105ba00
guiassuncao2007 <gui.assuncao2007@gmail.com>
2025-12-26 12:24:17 +0000

    Adicionei um botão em falta

--- pWeber.m
+++ pWeber.m
@@ -2,7 +2,7 @@ clear
 resposta=1;
 while ~(resposta==0)
     resposta=menu('Opções','Carregar dados','Mostrar dados',...
-        'Efetuar cálculos','Mostrar a localização','Sair do programa');
+        'Efetuar cálculos','Mostrar a localização','Gravar a localiazação','Sair do programa');
     switch resposta
         case 1 % o utilizador pode carregar noutro botões sem introduzir o nome do ficheiro, 
                % isso implica que o ficheiro não seja lido
@@ -15,6 +15,8 @@ while ~(resposta==0)
         case 4
             % Mostrar localização
         case 5
+            %Gravar localização
+        case 6
             return;
     end
 end
